da549870629feeed8d7932b246f68bbc23e0a0b6
Anc <anc136k@gmail.com>
2019-08-05 14:03:56 +0800

    test email

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-git config --global user.name "$NAME"
-git config --global user.email "$EMAIL"
+git config --global user.name "Anc"
+git config --global user.email "anc136k@gmail.com"
 
 sh -c "hexo g -d"
